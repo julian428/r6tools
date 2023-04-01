ca528c6011de1b8219790c1694882b0058786b6c
Julian Metzger <julianjometzger@gmail.com>
2023-04-01 14:35:28 +0200

    - fixed some errors

--- app/stats/[slug]/components/timePlayed.tsx
+++ app/stats/[slug]/components/timePlayed.tsx
@@ -5,10 +5,14 @@ interface Props {
 }
 
 export default function TimePlayed({ time }: Props) {
+  const timeIsCorrect = (time: string) => {
+    if (!time) return false;
+    return Number.isNaN(parseInt(time[0]));
+  };
   return (
     <section className="flex gap-2 items-center">
       <TimeIcon />
-      <p>{Number.isNaN(parseInt(time[0])) ? "-" : time}</p>
+      <p>{timeIsCorrect(time) ? "-" : time}</p>
     </section>
   );
 }
